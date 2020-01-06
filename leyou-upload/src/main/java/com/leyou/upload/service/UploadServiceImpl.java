98d85fb6c0c39069c7198c5c27e0c343030efe62
zhouxiaoqiang <827743509@qq.com>
2020-01-06 17:30:36 +0800

    改造了文件上传

--- leyou-upload/src/main/java/com/leyou/upload/service/UploadServiceImpl.java
+++ leyou-upload/src/main/java/com/leyou/upload/service/UploadServiceImpl.java
@@ -1,6 +1,10 @@
 package com.leyou.upload.service;
 
+import com.github.tobato.fastdfs.domain.StorePath;
+import com.github.tobato.fastdfs.domain.ThumbImageConfig;
+import com.github.tobato.fastdfs.service.FastFileStorageClient;
 import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -9,13 +13,16 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
 import java.util.Date;
-import java.util.List;
 import java.util.UUID;
 
 @Service
 public class UploadServiceImpl implements  UploadService {
+    @Autowired
+    private FastFileStorageClient storageClient;
+
+    @Autowired
+    private ThumbImageConfig thumbImageConfig;
     public String uploadImage(MultipartFile file) {
         try {
             //校验文件类型
@@ -24,8 +31,9 @@ public class UploadServiceImpl implements  UploadService {
             //校验文件内容
             BufferedImage bufferedImage = ImageIO.read(file.getInputStream());
             if(bufferedImage==null){return  null;}
-            //生成图片路径并保存
+
             String originalFilename = file.getOriginalFilename();
+              /*//生成图片路径并保存本地
             String suffix= "."+StringUtils.substringAfterLast(originalFilename,".");
             SimpleDateFormat simpleDateFormat=new SimpleDateFormat("/yyyy/MM/dd/");
             String format = simpleDateFormat.format(new Date());
@@ -36,7 +44,10 @@ public class UploadServiceImpl implements  UploadService {
             }
             String  filePath=format+ UUID.randomUUID().toString()+suffix;
             file.transferTo(new File("E:/leyouimage"+filePath));
-                     return "http://image.leyou.com"+filePath;
+            return "http://image.leyou.com"+filePath;*/
+            //生成图片并保存fastDFS
+            StorePath storePath = this.storageClient.uploadFile(file.getInputStream(), file.getSize(), StringUtils.substringAfterLast(originalFilename, "."), null);
+            return  "http://image.leyou.com/"+storePath.getFullPath();
         } catch (IOException e) {
             e.printStackTrace();
             return  null;
